2ea737cbfeca2b8551579f0032c40172c66c1433
Jonas Aklin <jonas.aklin@faigle.ch>
2022-06-05 21:44:15 +0200

    Added solution.

---
+++ src/ZombieGameDiceSim/ZombieGameDiceSim/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZombieGameDiceSim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/ZombieGameDiceSim/ZombieGameDiceSim/Rules.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace ZombieGameDiceSim
+{
+    /// <summary>
+    /// Implements basic game rules.
+    /// </summary>
+    internal static class Rules
+    {
+        public static int GetHits(int playerDice, int enemyDice)
+        {
+            if (playerDice > enemyDice)
+            {
+                return 1;
+            }
+
+            if (playerDice < enemyDice)
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+    }
+}
